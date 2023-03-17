76801fe300dd054a7d49a753a0c24bbbd2d94d03
Albert Lionelle <albert.lionelle@colostate.edu>
2023-03-16 22:14:07 -0400

    added student todo lines

--- converter.c
+++ converter.c
@@ -2,30 +2,16 @@
 
 AdjMatrix * convert_list_to_matrix(AdjList * list) {
     AdjMatrix * matrix = blank_matrix(list->size);
-    for (int i = 0; i < matrix->size; i++) {
-        for (int j = 0; j < matrix->size; j++) {
-            matrix->data[i][j] = 0;
-        }
-    }
-    for (int i = 0; i < list->size; i++) {
-        edge_t *edge = list->adj_lists[i];
-        while (edge != NULL) {
-            matrix->data[i][edge->vertex] = edge->weight;
-            edge = edge->next;
-        }
-    }
+    
+    // STUDENT TODO 
+
     return matrix;
 }
 
 AdjList * convert_matrix_to_list(AdjMatrix * matrix) {
     AdjList * list = create_graph(matrix->size);
     
-    for (int i = 0; i < matrix->size; i++) {
-        for (int j = 0; j < matrix->size; j++) {
-            if (matrix->data[i][j] > 0) {
-                add_edge(list, i, j, matrix->data[i][j]);
-            }
-        }
-    }
+    // STUDENT TODO
+    
     return list;
 }
